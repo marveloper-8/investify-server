3673fa8e03117f078a70ac6b9dbc3f71ed7494f1
unknown <dev@aoestatechariots.com>
2021-01-04 07:58:51 +0100

    user signup v2

--- config/keys/prod.js
+++ config/keys/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     MONGOURI: process.env.MONGOURI,
-    JWT_SECRET: process.env.JWT_SEC,
+    JWT_SECRET: process.env.JWT_SECRET,
     EMAIL: process.env.EMAIL,
     CONTACT_EMAIL: process.env.CONTACT_EMAIL,
     PASSWORD: process.env.PASSWORD,
